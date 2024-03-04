c08f5db3807bcf63003daf234c065ba81a45635d
HoangShiro <hoangdao13999@gmail.com>
2024-03-05 03:33:38 +0700

    test

--- utils/bot.py
+++ utils/bot.py
@@ -115,7 +115,7 @@ async def on_message(message):
     if not val.now_chat:
         val.set('now_chat', [chat])
     else:
-        val.now_chat = val.now_chat.extend(chat)
+        val.set('now_chat', val.now_chat.append(chat))
     if val.now_chat:
         if len(val.now_chat) >= 10:
             val.now_chat.pop(0)
