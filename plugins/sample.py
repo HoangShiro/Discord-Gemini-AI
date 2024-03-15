2bd8b69d44ad380ab3e42353dabafd3334f41d15
HoangShiro <hoangdao13999@gmail.com>
2024-03-15 22:18:13 +0700

    test

--- plugins/sample.py
+++ plugins/sample.py
@@ -5,7 +5,7 @@ from utils.bot import bot, val
 
 # Say
 @bot.slash_command(name="say", description=f"Để {val.ai_name} nói thay bạn.")
-async def loadplugin(interaction: discord.Interaction, text: str):
+async def user_say(interaction: discord.Interaction, text: str):
     if interaction.user.id != val.owner_uid: return await interaction.response.send_message(val.no_perm, ephemeral=True)
-    
+
     await interaction.response.send_message(text)
