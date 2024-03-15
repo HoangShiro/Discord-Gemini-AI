a008dbb61984894dcbab83f7c140623711a17e45
HoangShiro <hoangdao13999@gmail.com>
2024-03-15 23:17:23 +0700

    test

--- plugins/sample.py
+++ plugins/sample.py
@@ -1,15 +1,17 @@
 """ Mẫu plugin """
 
 import discord
+from discord.ext import commands
 from utils.bot import bot, val
 
+class cmds(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 # Say
-@bot.slash_command(name="say", description=f"Để {val.ai_name} nói thay bạn.")
-async def user_say(interaction: discord.Interaction, text: str):
-    if interaction.user.id != val.owner_uid: return await interaction.response.send_message(val.no_perm, ephemeral=True)
+    @bot.slash_command(name="say", description=f"Để {val.ai_name} nói thay bạn.")
+    async def user_say(interaction: discord.Interaction, text: str):
+        if interaction.user.id != val.owner_uid: return await interaction.response.send_message(val.no_perm, ephemeral=True)
 
-    await interaction.response.send_message(text)
+        await interaction.response.send_message(text)
 
-# Setup
-def setup(bot):
-    bot.add_command(user_say(bot))
+bot.add_cog(cmds(bot))
