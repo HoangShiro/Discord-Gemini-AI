7663660f7870fa2b231a222c83753960f55661a4
HoangShiro <hoangdao13999@gmail.com>
2024-03-25 11:57:16 +0700

    test

--- utils/bot.py
+++ utils/bot.py
@@ -291,20 +291,21 @@ async def on_message(message: discord.Message):
     if not val.public:
         if message.author.id != val.owner_uid: return
         if message.guild:
-            embed, view = await bot_notice(
-                tt="Chat mode: Private",
-                des=f"> Bật Public chat mode?",
-                footer=f"Bạn và mọi người có thể chat với {val.ai_name} ở Public chat mode.",
-                au_name=message.author.display_name,
-                au_avatar=message.author.display_avatar,
-                au_link=message.author.display_avatar,
-                public_btt=True,
-                )
-            return await message.channel.send(embed=embed, view=view)
+            if (val.ai_name.lower() or f"<@{bot.user.id}>") in message.content:
+                embed, view = await bot_notice(
+                    tt="Chat mode: Private",
+                    des=f"> Bật Public chat mode?",
+                    footer=f"Bạn và mọi người có thể chat với {val.ai_name} ở Public chat mode.",
+                    au_name=message.author.display_name,
+                    au_avatar=message.author.display_avatar,
+                    au_link=message.author.display_avatar,
+                    public_btt=True,
+                    )
+                return await message.channel.send(embed=embed, view=view)
     else:
-        if message.content:
-            if isinstance(message.channel, discord.DMChannel):
-                if message.author.id == val.owner_uid:
+        if isinstance(message.channel, discord.DMChannel):
+            if message.author.id == val.owner_uid:
+                if (val.ai_name.lower() or f"<@{bot.user.id}>") in message.content:
                     embed, view = await bot_notice(
                     tt="Chat mode: Public",
                     des=f"> Bật Private chat mode?",
@@ -314,7 +315,9 @@ async def on_message(message: discord.Message):
                     au_link=message.author.display_avatar,
                     private_btt=True,
                     )
-                return await message.channel.send(embed=embed, view=view)
+                    return await message.channel.send(embed=embed, view=view)
+            return
+        if message.content:
             val.set('ai_guild', message.guild.id)
             val.set('ai_channel', message.channel.id)
 
