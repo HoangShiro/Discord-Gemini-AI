0f227e30dcc131a59d65f322805d1470090e74df
HoangShiro <hoangdao13999@gmail.com>
2024-04-11 11:20:33 +0700

    test

--- utils/reply.py
+++ utils/reply.py
@@ -246,7 +246,7 @@ async def voice_send(url, ch):
         audio_source = FFmpegPCMAudio(url)
 
         # Play audio
-        ch.play(audio_source, after=lambda e: print(f'Player error: {e}' if e else None))
+        ch.play(audio_source, after=lambda e: print(f'Player error: {e}'))
 
     except Exception as e:
         print(f"{get_real_time()}> Send voice error: {e}")
