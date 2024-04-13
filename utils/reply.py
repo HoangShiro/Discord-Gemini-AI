0097b629437b78935e7641a097c5707c59093845
HoangShiro <hoangdao13999@gmail.com>
2024-04-13 23:37:58 +0700

    test

--- utils/reply.py
+++ utils/reply.py
@@ -777,7 +777,7 @@ async def cmd_msg():
             if not guild.voice_client:
                 await v_join_auto()
         
-        prompt = f"Returns the song/video/author names in the following chat if any: [{clear_chat}].\n If the chat does not contain song/video author names, return 'None'."
+        prompt = f"Returns the song/video and author names in the following chat if any: [{clear_chat}].\n If the chat does not contain song/video or author names, return 'None'."
         song_name = await mu.music_find(prompt=prompt)
         
         if song_name.lower() != "none":
