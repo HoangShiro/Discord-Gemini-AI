d04c5e90fff247d4eea3e71f4763ad83c1ad7abc
HoangShiro <hoangdao13999@gmail.com>
2024-04-14 22:45:00 +0700

    test

--- utils/ui.py
+++ utils/ui.py
@@ -1424,7 +1424,7 @@ async def xo_embed():
     if xo.draw: title = f"Hoà rồi! 💫"
     if xo.waiting and not xo.winner: des = "> Cần thêm 1 user nữa để bắt đầu!"
     if xo.in_match and not xo.ai_match: des = ""
-    elif xo.in_match and xo.ai_match: des = val.now_chat
+    elif xo.in_match and xo.ai_match: des = val.now_chat_ai
     
     board = xo.icon()
     board = f"{xo.icon()[0][0]}{xo.iconB1}{xo.icon()[0][1]}{xo.iconB1}{xo.icon()[0][2]}\n{xo.iconB1}{xo.iconB2}{xo.iconB1}{xo.iconB2}{xo.iconB1}\n{xo.icon()[1][0]}{xo.iconB1}{xo.icon()[1][1]}{xo.iconB1}{xo.icon()[1][2]}\n{xo.iconB1}{xo.iconB2}{xo.iconB1}{xo.iconB2}{xo.iconB1}\n{xo.icon()[2][0]}{xo.iconB1}{xo.icon()[2][1]}{xo.iconB1}{xo.icon()[2][2]}"
