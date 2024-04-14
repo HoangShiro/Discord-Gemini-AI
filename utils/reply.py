9e3a0300289cb310e0d53e9386d8d27288b60293
HoangShiro <hoangdao13999@gmail.com>
2024-04-14 15:38:35 +0700

    test

--- utils/reply.py
+++ utils/reply.py
@@ -819,7 +819,7 @@ async def cmd_msg():
             except Exception as e: return None
         
         keywords = await _get_keywords()
-        keywords = keywords.lower().replace(" ", "_")
+        keywords = keywords.strip().lower().replace(" ", "_").replace("\n", " ")
         if val.cmd_csl: print(f"{get_real_time()}> Art keyword: ", keywords)
         if keywords:
             async def _start_search():
