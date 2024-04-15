80a7c98a8b944958b20ffa7c61ac67aa236acf81
HoangShiro <hoangdao13999@gmail.com>
2024-04-15 11:07:39 +0700

    test

--- utils/ui.py
+++ utils/ui.py
@@ -1436,10 +1436,10 @@ async def xo_embed():
     
     board = xo.icon()
     board = f"""{xo.icon()[0][0]}{xo.iconB1}{xo.icon()[0][1]}{xo.iconB1}{xo.icon()[0][2]}
-                {xo.iconB1}{xo.iconB2}{xo.iconB1}{xo.iconB2}{xo.iconB1}
-                {xo.icon()[1][0]}{xo.iconB1}{xo.icon()[1][1]}{xo.iconB1}{xo.icon()[1][2]}
-                {xo.iconB1}{xo.iconB2}{xo.iconB1}{xo.iconB2}{xo.iconB1}
-                {xo.icon()[2][0]}{xo.iconB1}{xo.icon()[2][1]}{xo.iconB1}{xo.icon()[2][2]}"""
+{xo.iconB1}{xo.iconB2}{xo.iconB1}{xo.iconB2}{xo.iconB1}
+{xo.icon()[1][0]}{xo.iconB1}{xo.icon()[1][1]}{xo.iconB1}{xo.icon()[1][2]}
+{xo.iconB1}{xo.iconB2}{xo.iconB1}{xo.iconB2}{xo.iconB1}
+{xo.icon()[2][0]}{xo.iconB1}{xo.icon()[2][1]}{xo.iconB1}{xo.icon()[2][2]}"""
     
     notice = xo.notice
     
